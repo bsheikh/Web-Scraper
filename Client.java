7940413f81572cfb9d2b6af7644decc7f941231a
Bill Sheikh <bsheikhp@gmail.com>
2016-02-04 17:21:21 -0500

    Finished Prasing data

--- Client.java
+++ Client.java
@@ -1,6 +1,30 @@
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 
 public class Client {
-	
 
+	public Client() {}
+
+	public URLConnection connectToWebsite(String websiteURL) {
+
+		URL url;
+		URLConnection connection = null;
+		
+		try {
+			url = new URL(websiteURL);
+			connection = url.openConnection();
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return connection;
+	}
+	
+	
+	
+	
 
 }
--- WebStats.java
+++ WebStats.java
@@ -1,27 +1,16 @@
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLConnection;
-
 public class WebStats {
 	public static void main(String[] args) {
-		
-		URL url;
-		Phaser phaser = new Phaser();
-		
 
-      try {
-      	
-      	String a="http://www.ryerson.ca/index.html";
-      	url = new URL(a);
-      	URLConnection connection = url.openConnection();
-      	phaser.pharseDocument(connection);
-
-      	
-      	} catch (MalformedURLException e) {
-      		e.printStackTrace();
-      	} catch (IOException e) {
-      		e.printStackTrace();
-      	}
+		// get content from user
+		// if no content, use default numbers
+		// call the Client and get connection
+		
+		Praser phaser = new Praser();
+		Client HTTPClient = new Client();
+		phaser.pharseDocument(HTTPClient.connectToWebsite("http://www.ryerson.ca/index.html"));
+		phaser.printHtmlTags();
 	}
+	
+	
+	
 }
